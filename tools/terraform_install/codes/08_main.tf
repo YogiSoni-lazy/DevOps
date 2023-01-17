71778b50f75c60047f1d841cd28518622e3ebb08
Yogita Soni <YogiSoni-lazy>
2023-01-17 18:04:47 +0530

    changes

--- tools/terraform_install/codes/08_main.tf
+++ tools/terraform_install/codes/08_main.tf
@@ -2,16 +2,3 @@ resource "aws_instance" "newwebserver" {
         ami = "${lookup(var.AMI,var.AWS_REGION)}"
         instance_type = "${var.AWS_INSTANCE_TYPE}"
 }
-
-output "public_ip" {
-	#value = "${aws_instance.newwebserver.public_ip}"
-	value =  aws_instance.newwebserver.public_ip
-}
-output "cpu" {
-	#value = "${aws_instance.newwebserver.public_ip}"
-	value =  aws_instance.newwebserver.cpu_core_count
-}
-output "private_ip" {
-	#value = "${aws_instance.newwebserver.public_ip}"
-	value =  aws_instance.newwebserver.private_ip
-}
